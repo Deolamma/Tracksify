67189f0576264c47a7965e3b510886187fe26481
Bukolaope <oluwabukolaope@gmail.com>
2024-01-13 17:07:51 +0100

    implementation of employee project update for employer

--- frontend/tracksify/app/(routes)/(employer)/employer-project/page.tsx
+++ frontend/tracksify/app/(routes)/(employer)/employer-project/page.tsx
@@ -2,10 +2,12 @@
 import Logo from "@/components/logo";
 import Link from "next/link";
 import Modal from "@/components/modal";
+import { useRouter } from "next/navigation";
 import { Fragment, useState } from "react";
 import { DatePicker } from "@/components/datepicker";
 
 import React from "react";
+import router from "next/router";
 
 const EmployerProjectDetails = () => {
   const [showModal, setShowModal] = useState(false);
@@ -43,10 +45,46 @@ const EmployerProjectDetails = () => {
                 <div className="flex space-x-5">
                   {" "}
                   <div className="w-full">
-                    <DatePicker label={"Start Time"} />
+                    <DatePicker
+                      label={"Start Time"}
+                      icon={
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          className=" w-5 h-5"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
+                          />
+                        </svg>
+                      }
+                    />
                   </div>{" "}
                   <div className="w-full">
-                    <DatePicker label={"End Time"} />
+                    <DatePicker
+                      label={"End Time"}
+                      icon={
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          className=" w-5 h-5"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
+                          />
+                        </svg>
+                      }
+                    />
                   </div>{" "}
                 </div>
                 <div className="mb-6 mt-6">
@@ -94,11 +132,10 @@ const EmployerProjectDetails = () => {
                 </h3>
 
                 {/* GRID 1 */}
-                <Link
-                  href="/employer-dashboard/project-details/1"
-                  className=" hover:bg-color_hover p-5 cursor-pointer"
-                >
-                  Project 1
+                <Link href="/employer-project/12345/list-of-projects">
+                  <p className=" hover:bg-color_hover p-5 cursor-pointer">
+                    Project 1
+                  </p>
                 </Link>
 
                 <p className=" hover:bg-color_hover p-5">Dec 1, 2023</p>
